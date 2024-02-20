c1e6cb97f7eee46960789f8fffdb02014a94ff29
Sarfaraz Shaikh <154805542+shahmeershaikh@users.noreply.github.com>
2024-02-20 12:57:57 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,14 @@
+let myname = "Shahmeer Shaikh";
+console.log(myname);
+console.log(myname);
+console.log(myname);
+console.log(myname);
+console.log(myname);
+console.log(myname);
+console.log(myname);
+console.log(myname);
+console.log(myname);
+// let age= 50;
+
+console.log(6 + 10);
+console.log(" 5 + 10 ");
